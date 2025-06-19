b11b892d07ab258447e078c2b7b87b2e7c3dce32
danielsierraaraujo <daniel.sierra@udla.edu.ec>
2025-06-19 08:35:23 -0500

    segundo commit - agregamos la geolocalizacion y weather usando la api para que nos de los datos

---
+++ HerreraSierraVargasAppApuntes2/Services/GeolocationServices.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerreraSierraVargasAppApuntes2.Services
+{
+    class GeolocationServices
+    {
+        private CancellationTokenSource _cancelTokenSource;
+        private bool _isCheckingLocation;
+
+        public async Task<Location> GetCurrentLocation()
+        {
+            try
+            {
+                _isCheckingLocation = true;
+
+                GeolocationRequest request = new GeolocationRequest(GeolocationAccuracy.Medium, TimeSpan.FromSeconds(10));
+
+                _cancelTokenSource = new CancellationTokenSource();
+
+                Location location = await Geolocation.Default.GetLocationAsync(request, _cancelTokenSource.Token);
+
+                if (location != null)
+                {
+                    return location;
+                }
+                //throw new Exception("Unable to get location");
+
+            }
+            // Catch one of the following exceptions:
+            //   FeatureNotSupportedException
+            //   FeatureNotEnabledException
+            //   PermissionException
+            catch (Exception ex)
+            {
+                // Unable to get location
+            }
+            finally
+            {
+                _isCheckingLocation = false;
+            }
+            return new Location
+            {
+                Latitude = 0,
+                Longitude = 0,
+            };
+        }
+
+        public void CancelRequest()
+        {
+            if (_isCheckingLocation && _cancelTokenSource != null && _cancelTokenSource.IsCancellationRequested == false)
+                _cancelTokenSource.Cancel();
+        }
+    }
+}
---
+++ HerreraSierraVargasAppApuntes2/Services/WeatherServices.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerreraSierraVargasAppApuntes2.Services
+{
+    public class WeatherServices
+    {
+        public async Task<WeatherData> GetCurrentLocationWeatherAsync()
+        {
+            GeolocationServices geolocationServices = new GeolocationServices();
+            Location location = await geolocationServices.GetCurrentLocation();
+            return await GetWeatherDataFromLocationAsync(location.Latitude, location.Longitude);
+        }
+        public async Task<WeatherData> GetWeatherDataFromLocationAsync(double latitude, double longitude)
+        {
+            string latitude_str = latitude.ToString().Replace(",", ".");
+            string longitude_str = longitude.ToString().Replace(",", ".");
+
+            string url = $"https://api.open-meteo.com/v1/forecast?latitude={latitude_str}&longitude={longitude_str}&current=temperature_2m,relative_humidity_2m,rain";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                var result = await response.Content.ReadAsStringAsync();
+                WeatherData data = JsonConvert.DeserializeObject<WeatherData>(result);
+                return data;
+            }
+        }
+    }
+}
