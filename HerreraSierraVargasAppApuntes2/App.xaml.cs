e379cdba59da189855b4f597f3ea106d14d29021
laboratorio <laboratorio@UPOAULA10711>
2025-06-19 08:21:22 -0500

    Agregar archivos de proyecto.

---
+++ HerreraSierraVargasAppApuntes2/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HerreraSierraVargasAppApuntes2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
