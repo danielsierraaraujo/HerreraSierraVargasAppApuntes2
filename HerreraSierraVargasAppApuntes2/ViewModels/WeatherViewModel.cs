9d4afb856624d19a911419e80f56a7ca23892d92
danielsierraaraujo <daniel.sierra@udla.edu.ec>
2025-06-19 08:39:05 -0500

    Tercer commit - Este código obtiene y actualiza datos del clima según la ubicación del usuario.

---
+++ HerreraSierraVargasAppApuntes2/ViewModels/WeatherViewModel.cs
@@ -0,0 +1,50 @@
+﻿using HerreraSierraVargasAppApuntes2.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HerreraSierraVargasAppApuntes2.ViewModels
+{
+    class WeatherViewModel : INotifyPropertyChanged
+    {
+        private WeatherData _weatherData = new WeatherData();
+        public WeatherData WeatherDataInfo
+        {
+            get => _weatherData;
+            set
+            {
+                if (_weatherData != value)
+                {
+                    _weatherData = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public ICommand RecalculateWeather { get; set; }
+        public WeatherViewModel()
+        {
+            GetCurrentWeatherFromLocation();
+            RecalculateWeather = new Command(async () => GetWeatherFromLocation());
+        }
+        public async void GetCurrentWeatherFromLocation()
+        {
+            WeatherServices weatherServices = new WeatherServices();
+            WeatherDataInfo = await weatherServices.GetCurrentLocationWeatherAsync();
+        }
+
+        public async void GetWeatherFromLocation()
+        {
+            WeatherServices weatherServices = new WeatherServices();
+            WeatherDataInfo = await weatherServices.GetWeatherDataFromLocationAsync(_weatherData.latitude, _weatherData.longitude);
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string name = "") =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
