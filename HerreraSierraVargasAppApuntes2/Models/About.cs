8deb21d43a18c09b5c4da44133af5a9ffa8dd055
danielsierraaraujo <daniel.sierra@udla.edu.ec>
2025-06-19 08:30:16 -0500

    Primer Commit - Empezamos Creando los models para cada una de las pestañas

---
+++ HerreraSierraVargasAppApuntes2/Models/About.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerreraSierraVargasAppApuntes2.Models
+{
+    internal class About
+    {
+        public string Name => "Daniel Sierra, Ricardo Herrera & Pablo Vargas";
+    }
+}
