41c56603cc519652c862a681a93e9cad77248dab
RicardoHerrera2004 <ricardo.herrera.acurio@udla.edu.ec>
2025-06-25 22:15:53 -0500

    AboutPage: Se termino de completar la pagina about, la card view, las imagenes y descripciones

--- HerreraSierraVargasAppApuntes2/Views/CardView.xaml.cs
+++ HerreraSierraVargasAppApuntes2/Views/CardView.xaml.cs
@@ -1,6 +1,6 @@
 namespace HerreraSierraVargasAppApuntes2.Views;
 
-public partial class CardView : ContentPage
+public partial class CardView : ContentView
 {
     public CardView()
     {
@@ -11,8 +11,8 @@ public partial class CardView : ContentPage
     public static readonly BindableProperty CardTitleProperty =
         BindableProperty.Create(nameof(CardTitle), typeof(string), typeof(CardView), string.Empty);
 
-    public static readonly BindableProperty CardDescriptionProperty =
-        BindableProperty.Create(nameof(CardDescription), typeof(string), typeof(CardView), string.Empty);
+    public static readonly BindableProperty CardDeporteProperty =
+        BindableProperty.Create(nameof(CardDeporte), typeof(string), typeof(CardView), string.Empty);
 
     public static readonly BindableProperty IconImageSourceProperty =
         BindableProperty.Create(nameof(IconImageSource), typeof(ImageSource), typeof(CardView), default(ImageSource));
@@ -20,16 +20,25 @@ public partial class CardView : ContentPage
     public static readonly BindableProperty BorderColorProperty =
         BindableProperty.Create(nameof(BorderColor), typeof(Color), typeof(CardView), Colors.Gray);
 
+    public static readonly BindableProperty AgeProperty =
+    BindableProperty.Create(nameof(Age), typeof(string), typeof(CardView), string.Empty);
+
+    public string Age
+    {
+        get => (string)GetValue(AgeProperty);
+        set => SetValue(AgeProperty, value);
+    }
+
     public string CardTitle
     {
         get => (string)GetValue(CardTitleProperty);
         set => SetValue(CardTitleProperty, value);
     }
 
-    public string CardDescription
+    public string CardDeporte
     {
-        get => (string)GetValue(CardDescriptionProperty);
-        set => SetValue(CardDescriptionProperty, value);
+        get => (string)GetValue(CardDeporteProperty);
+        set => SetValue(CardDeporteProperty, value);
     }
 
     public ImageSource IconImageSource
